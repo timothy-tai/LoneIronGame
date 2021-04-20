940b2286c45e456d2df25c928cbf405bc93ab91a
timothy-tai <titai@ucsc.edu>
2021-04-19 22:57:28 -0700

    Intial Commit/Changes

---
+++ RocketPatrolMods/src/prefabs/Rocket.js
@@ -0,0 +1,35 @@
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        this.movementSpeed = 3;
+        this.isFiring = false;
+        this.sfxRocket = scene.sound.add('sfx_rocket');
+    }
+
+    update() {
+        if(this.isFiring) {
+            this.y -= this.movementSpeed;
+            if(this.y < borderUISize*3) {
+                this.y = game.config.height-borderUISize-borderPadding;
+                this.isFiring = false;
+            }
+        } else {
+            if(keyLEFT.isDown) {
+                this.x -= 2;
+            }
+            if(keyRIGHT.isDown) {
+                this.x += 2;
+            }
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+                this.sfxRocket.play();
+            }
+            this.x = Phaser.Math.Clamp(this.x, borderUISize + borderPadding, game.config.width-borderUISize-borderPadding);
+        }
+    }
+
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
+    }
+}
