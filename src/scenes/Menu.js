6455320c8c751749b43ea8412455ceceaa40a12e
timothy-tai <titai@ucsc.edu>
2021-04-20 02:08:42 -0700

    Added Custom Font, adjusted color scheme

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,17 +5,19 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
+        this.load.image('starfield', 'assets/starfield.png');
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
 
     create() {
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         let menuConfig = {
-            fontFamily: 'Courier',
+            fontFamily: 'CustomFont',
             fontSize: '20px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            //backgroundColor: '#F3B141',
+            color: '#FFFFFF',
             align: 'right',
             padding: {
               top: 5,
@@ -23,17 +25,17 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width/2, game.config.height/2-borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
+        this.add.text(game.config.width/2, game.config.height/3-borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move and (F) to fire', menuConfig).setOrigin(0.5);
+        
+        //menuConfig.backgroundColor = '#00FF00';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
-    
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
+        this.starfield.tilePositionX -= 4;
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
           // easy mode
           game.settings = {
